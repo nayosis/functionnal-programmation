bb0e14fceb1d8eb77ee1ad3f5b4729a4fef6ce8b
Vincent Gouadon <vincent.gouadon@live.fr>
2019-04-11 06:06:57 +0200

    add tests on getById

--- src/test/java/com/serli/workshop/functionnalprogrammation/service/SerliansServiceTest.java
+++ src/test/java/com/serli/workshop/functionnalprogrammation/service/SerliansServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,7 +26,24 @@ public class SerliansServiceTest {
     @Test
     public void shouldGetSerlians() {
         List<Serlian> serlians = service.getSerlians();
-        assertThat(serlians).hasSize(18);
+        assertThat(serlians).hasSize(199);
     }
+
+    @Ignore
+    @Test
+    public void shouldGetOneSerlians() {
+        Optional<Serlian> serlianById = service.getSerlianById("193cca81-3cee-4866-89ee-82f7f06a24d2");
+        assertThat(serlianById).isNotEmpty();
+        assertThat(serlianById).map(serlian -> serlian.getNom()).hasValue("Valentine");
+    }
+
+    @Ignore
+    @Test
+    public void shouldGetOneSerlians_NotFound() {
+        Optional<Serlian> serlianById = service.getSerlianById("193cxxx-3cee-4866-89ee-82f7f06a24d2");
+        assertThat(serlianById).isNotNull();
+        assertThat(serlianById).isEmpty();
+    }
+
 
 }
